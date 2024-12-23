0f9d0bc1fa6c49818eae74b8ac45dc422c59a7b5
54454 <544548586@qq.com>
2024-12-23 15:30:37 +0800

    Initial commit

---
+++ src/test/java/cn/lanqiao/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
